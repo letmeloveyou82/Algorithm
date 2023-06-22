517f964d1d82010a78f0b987bfec6381b34554fd
Yunjeong Choi <shineanl04@gmail.com>
2023-06-22 17:34:45 +0900

    [BOJ] 2178번 - 미로 탐색

---
+++ Python/BOJ/DFS & BFS/2178.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
+# 이동할 4 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# bfs 
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
