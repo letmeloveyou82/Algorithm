18cf47f52e7f6d1cd7a46c0f7b329992b9ea103c
Yunjeong Choi <shineanl04@gmail.com>
2023-07-13 23:29:39 +0900

    [프로그래머스] 연습문제 - x만큼 간격이 있는 n개의 숫자

---
+++ Python/Programmers/연습문제/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,11 @@
+def solution(x, n):
+    answer = []
+    if x == 0:
+        return [0] * n
+    elif x > 0:
+        for i in range(x, x*n+1, x):
+            answer.append(i)
+    else:
+        for i in range(x, x*n-1, x):
+            answer.append(i)
+    return answer
