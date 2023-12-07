93f62c7008c9832bfa22ce2947285d4c9a3de290
Yunjeong Choi <shineanl04@gmail.com>
2023-12-07 23:18:14 +0900

    [BOJ] 2579번 - 계단 오르기

---
+++ Python/BOJ/DP/2579.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+score = [0] * 301
+for i in range(1, n+1):
+    score[i] = int(input())
+
+dp = [0] * 301
+dp[1] = score[1]
+dp[2] = score[1] + score[2]
+dp[3] = max(score[1]+score[3], score[2]+score[3])
+
+for i in range(4, n+1):
+    dp[i] = max(dp[i-3]+score[i-1]+score[i], dp[i-2]+score[i])
+print(dp[n])
