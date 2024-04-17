eb33dd740aa830914248bba15e46b7e545cb7bf6
Yunjeong Choi <shineanl04@gmail.com>
2024-04-17 17:28:44 +0900

    [BOJ] 13144번 - List of Unique Numbers

---
+++ Python/BOJ/투 포인터/13144.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+num_list = list(map(int, input().split()))
+result = 0
+start, end = 0, 0
+seq = [False for _ in range(100001)]
+
+while start < n and end < n:
+    if not seq[num_list[end]]:      # start부터 end까지 중복 숫자 없으면
+        seq[num_list[end]] = True
+        end += 1
+        result += (end - start)     # end를 포함하여 만들 수 있는 수열의 개수
+    else:
+        seq[num_list[start]] = False
+        start += 1
+
+print(result)
