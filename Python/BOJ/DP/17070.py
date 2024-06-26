dd119d42a6defc9c808eb921ec8b71cfc79f6bc3
Yunjeong Choi <shineanl04@gmail.com>
2024-06-26 17:10:30 +0900

    [BOJ] 17070번 - 파이프 옮기기 1

---
+++ Python/BOJ/DP/17070.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+# [파이프종류][r위치][c위치]
+# 0 : 가로, 1 : 대각선, 2 : 세로
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+
+dp[0][0][1] = 1 # 가로 방향 시작 파이프의 끝 부분 위치
+
+# 1행 미리 처리
+for i in range(2, N):
+    if board[0][i] == 0:
+        dp[0][0][i] = dp[0][0][i-1]
+
+for r in range(1, N):
+    for c in range(1, N):
+        # 대각선 파이프 추가
+        if board[r][c] == 0 and board[r][c-1] == 0 and board[r-1][c] == 0:
+            dp[1][r][c] = dp[0][r-1][c-1] + dp[1][r-1][c-1] + dp[2][r-1][c-1]
+
+        # 가로, 세로 파이프 추가
+        if board[r][c] == 0:
+            dp[0][r][c] = dp[0][r][c-1] + dp[1][r][c-1]
+            dp[2][r][c] = dp[2][r-1][c] + dp[1][r-1][c]
+
+print(sum(dp[i][N-1][N-1] for i in range(3)))
