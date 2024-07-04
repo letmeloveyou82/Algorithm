fee5c8856338c248022109fe9c02d4c1ef77b09d
Yunjeong Choi <shineanl04@gmail.com>
2024-07-04 16:08:51 +0900

    [BOJ] 11052번 - 카드 구매하기

---
+++ Python/BOJ/DP/11052.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+P = [0] + list(map(int, input().split()))
+dp = [0] * (N+1)
+
+for i in range(1, N+1):
+    for k in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-k] + P[k])
+
+print(dp[i])
