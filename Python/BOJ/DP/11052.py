7c2cfc58b6a276ea05a90967983780063512b50b
letmeloveyou82 <shineanl04@gmail.com>
2025-07-08 18:19:43 +0900

    [BOJ] 11052번 - 카드 구매하기
    DP

--- Python/BOJ/DP/11052.py
+++ Python/BOJ/DP/11052.py
@@ -2,11 +2,15 @@ import sys
 
 input = sys.stdin.readline
 N = int(input())
-P = [0] + list(map(int, input().split()))
+P = [-1] + list(map(int, input().split()))
+
 dp = [0] * (N+1)
+dp[1] = P[1]
 
-for i in range(1, N+1):
-    for k in range(1, i+1):
-        dp[i] = max(dp[i], dp[i-k] + P[k])
+for i in range(2, N+1):
+    dp[i] = P[i]
+    for j in range(1, i//2+1):
+        if dp[i] < dp[j] + dp[i-j]:
+            dp[i] = dp[j] + dp[i-j]
 
-print(dp[i])
+print(dp[N])
