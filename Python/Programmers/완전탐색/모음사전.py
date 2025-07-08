66d361d5eb044ca73682b72988d42cebedd9310d
letmeloveyou82 <shineanl04@gmail.com>
2025-07-08 18:17:34 +0900

    [프로그래머스] 완전탐색 - 모음 사전

--- Python/Programmers/완전탐색/모음사전.py
+++ Python/Programmers/완전탐색/모음사전.py
@@ -1,12 +1,12 @@
 from itertools import product
+
 def solution(word):
-    alpha = ['A', 'E', 'I', 'O', 'U']
-    case = []
+    vowel = ['A', 'E', 'I', 'O', 'U']
+    word_list = []
+    
     for i in range(1, 6):
-        for j in product(alpha, repeat = i):
-            temp = ''
-            for k in j:
-                temp += k
-            case.append(temp)
-    case.sort()
-    return case.index(word)+1
+        for case in list(product(vowel, repeat=i)):
+            word_list.append("".join(case))
+    word_list.sort()
+    
+    return word_list.index(word)+1
