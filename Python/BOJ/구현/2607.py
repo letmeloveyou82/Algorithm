68c908869696272d70453ef12d64620114cddb1e
letmeloveyou82 <shineanl04@gmail.com>
2025-07-10 00:15:51 +0900

    [BOJ] 2607번 - 비슷한 단어

--- Python/BOJ/구현/2607.py
+++ Python/BOJ/구현/2607.py
@@ -3,25 +3,28 @@ from collections import Counter
 
 input = sys.stdin.readline
 N = int(input())
-first_word = input().rstrip()
+words = list(input().rstrip() for _ in range(N))
+
+first_word = words[0]
+first_word_cnt = Counter(first_word)
 answer = 0
 
-for _ in range(N-1):
-    first_word_component = Counter(first_word)
-    now_word = input().rstrip()
-    now_word_component = Counter(now_word)
-    if first_word_component == now_word_component:
+for i in range(1, N):
+    target_cnt = Counter(words[i])
+    diff = first_word_cnt - target_cnt
+    reverse_diff = target_cnt - first_word_cnt
+
+    total_diff = sum(diff.values()) + sum(reverse_diff.values())
+    len_diff = abs(len(first_word) - len(words[i]))
+
+    if len_diff > 1:
+        continue
+
+    if len_diff == 0 and total_diff == 0: # 같은 구성을 가진 경우
         answer += 1
-    else:
-        similar_word = True
-        first_word_component.subtract(now_word_component)
-        diff = list(first_word_component.values())
-        for i in diff: # 한 문자 교체해도 비슷한 단어 안 되는 경우
-            if i <= -2 or i >= 2:
-                similar_word = False
-                break
-        if diff.count(1) >= 2 or diff.count(-1) >= 2: # 한 문자 더하거나 빼도 비슷한 단어 안 되는 경우
-            similar_word = False
-        if similar_word:
-            answer += 1
+    elif len_diff == 0 and total_diff == 2: # 글자 하나 다른 경우
+        answer += 1
+    elif len_diff == 1 and total_diff == 1: # 글자 하나 추가 또는 삭제
+        answer += 1
+
 print(answer)
