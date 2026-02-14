bb5e52be0d3ea17a9f7f20772404d7c6464d9e86
letmeloveyou82 <shineanl04@gmail.com>
2026-02-14 20:15:50 +0900

    [BOJ] 20922번 - 겹치는 건 싫어

--- Python/BOJ/투 포인터/20922.py
+++ Python/BOJ/투 포인터/20922.py
@@ -1,20 +1,23 @@
 import sys
+from collections import defaultdict
 
 input = sys.stdin.readline
+
 N, K = map(int, input().split())
-num = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
-max_length = 0
-left, right = 0, 0
-num_count = [0] * (max(num)+1)
+cnt = defaultdict(int)
+l = 0
+ans = 0
 
-while right < N:
-    if num_count[num[right]] < K:
-        num_count[num[right]] += 1
-        right += 1
-        max_length = max(max_length, right - left)
-    else:
-        num_count[num[left]] -= 1
-        left += 1
+for r in range(N):
+    cnt[a[r]] += 1
 
-print(max_length)
+    # a[r]가 K 초과이면, 다시 K 이하가 될 때까지 L 이동
+    while cnt[a[r]] > K:
+        cnt[a[l]] -= 1
+        l += 1
+
+    ans = max(ans, r-l+1)
+
+print(ans)
